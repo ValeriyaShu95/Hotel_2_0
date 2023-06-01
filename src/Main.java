c197ca23ee97d28f84c20a7b1a02c457c48dc282
valeriasusenaceva <valerapopova55@gmail.com>
2023-06-01 23:47:40 +0500

    allMoney

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Client client1 = new Client("Max", "male", "+7131332", "fg@njd.ru", 2000);
-        Client client2 = new Client("Lera", "female", "+71232442", "jfjf@gdjgn.ru", 1000);
 
-        Room room1 = new Room(1, 2,"male", 200, false);
-        Room[] roomsHotel1 = new Room[]{room1};
+        Client client1 = new Client("Max", "male", "+7131332", "fg@njd.ru", 290);
+        Client client2 = new Client("Lera", "female", "+71232442", "jfjf@gdjgn.ru", 10);
+        Client [] clients = new Client[]{client1, client2};
+
+        Room hotel1room1 = new Room(1, 2,"male", 200, false);
+        Room[] roomsHotel1 = new Room[]{hotel1room1};
         Hotel hotel1 = new Hotel("Vagas", roomsHotel1);
 
         Room[] roomsHotel2 = new Room[]{};
@@ -13,7 +15,10 @@ public class Main {
         Room[] roomsHotel3 = new Room[]{};
         Hotel hotel3 = new Hotel("Vagas", roomsHotel3);
 
-        Methods.transaction(client1, room1);
+        Hotel [] hotelsArray = new Hotel[]{hotel1, hotel2, hotel3};
+
+        //Methods.transaction(client1, hotel1room1);
+        Methods.allMoney(clients);
 
 
     }
